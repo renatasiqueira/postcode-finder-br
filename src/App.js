96ddd7a38fee0df5c7f4467b98da1cd956cf01ac
unknown <renatabssiqueira@gmail.com>
2022-06-08 21:41:35 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
+import '../src/style.css'
+
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('')
+  const [zip, setZip] = useState({});
+
+  async function handleSearch(){
+    
+    if(input == ''){
+      alert("Your Postcode/Zip")
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`)
+      setZip(response.data)
+      setInput("");
+
+    }catch{
+      alert("Oops...error");
+      setInput("")
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Postcode Finder</h1>
+      <h6 className='subtitle'>Brazil</h6>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Start typing the address..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+        {Object.keys(zip).length > 0 && (
+          <main className='main'>
+            <h2>CODEPOST: {zip.cep}</h2>
+
+            <span>{zip.logradouro}</span>
+            <span>{zip.complemento}</span>
+            <span>{zip.bairro}</span>
+            <span>{zip.localidade} - {zip.uf}</span>
+
+          </main>
+        )}
+      
     </div>
   );
 }
